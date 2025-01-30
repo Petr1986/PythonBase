f0b5d50b1072b9e281d7a72c016381c63fa09626
[petr] <[petr.art.m@gmail.com]>
2025-01-30 10:18:41 +0300

    remove pycache

--- tests/test_celsius_to_farenheit.py
+++ tests/test_celsius_to_farenheit.py
@@ -1,16 +1,14 @@
 from homework1.celsius_to_fahrenheit import conversion
 import pytest
 
-@pytest.mark.parametrize("x, expected_result", [(-21, -5),
-                                                (4, 39),
-                                                (-11, 12),
-                                                (43, 109)])
+
+@pytest.mark.parametrize("x, expected_result", [
+    (-21, -5), (4, 39), (-11, 12), (43, 109)])
 def test_conversion(x, expected_result):
     assert conversion(x) == expected_result
 
-def test_conversation_out_of_range():
-    with pytest.raises(ValueError, match=r"Temperature .* is out of range"):
-        conversion(-300)
 
-    with pytest.raises(ValueError, match=r"Temperature .* is out of range"):
-        conversion(1500)
+@pytest.mark.parametrize("x", [-300, 1500])
+def test_conversation_out_of_range(x):
+    with pytest.raises(ValueError, match=f"Temperature {x} is out of range"):
+        conversion(x)
